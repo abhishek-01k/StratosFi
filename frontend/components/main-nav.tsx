af8b6c3a82457063482721e46ce604501ec417c8
abhishek-01k <abhishek@push.org>
2025-08-02 21:58:41 +0530

    Fixed logo

--- frontend/components/main-nav.tsx
+++ frontend/components/main-nav.tsx
@@ -5,6 +5,7 @@ import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/util/utils"
 import { Icons } from "@/components/icons"
+import Image from "next/image"
 
 interface MainNavProps {
   items?: NavItem[]
@@ -14,7 +15,12 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
-        <Icons.logo className="size-6" />
+        <Image
+          src="/1inch_token.png"
+          alt="1inch_logo"
+          width={40}
+          height={40}
+        />
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
