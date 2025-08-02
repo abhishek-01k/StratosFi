bbe02811068e1444971ea3640d7964908f67de53
abhishek-01k <abhishekkumar214567@gmail.com>
2025-08-02 23:09:29 +0530

    stratosfi

--- frontend/util/wagmi.ts
+++ frontend/util/wagmi.ts
@@ -2,8 +2,8 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { mainnet, arbitrum, polygon, optimism, base } from 'wagmi/chains'
 
 export const config = getDefaultConfig({
-  appName: '1inch Advanced Strategies',
-  projectId: 'YOUR_PROJECT_ID', // Get from https://cloud.walletconnect.com
+  appName: 'StratosFi',
+  projectId: '0f869a1f7240141b3408d5d1fe42545a', // Get from https://cloud.walletconnect.com
   chains: [mainnet, arbitrum, polygon, optimism, base],
   ssr: true,
 })
